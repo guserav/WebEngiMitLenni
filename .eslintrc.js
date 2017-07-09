fd8a2970888153a5d832e1234115c60e00d063b7
guserav <erik.zeiske@web.de>
2017-07-10 00:45:16 +0200

    Adjust eslintConfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,13 @@
 module.exports = {
-    "globals":{
-        "module":true,
-        "require":true
-    },
     "extends": "eslint:recommended",
     "env":{
         "es6": true,
-        "browser": true
+        "browser": true,
+        "commonjs": true
     },
     "rules": {
         "indent": ["error", 4],
         "semi": ["error", "always"],
-
         "no-console": "off"
     }
 };
