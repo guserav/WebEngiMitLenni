b31f86449a9d6bacf3111a6f3b185e0540e180ad
guserav <erik.zeiske@web.de>
2017-07-11 22:35:26 +0200

    Refactoring of code and adding unread counter and automatic update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 const textConverter = require('./textConversion');
-
 const apiurl = "http://danielrutz.de:3000/api";
-let username = "user12"; //müssen wir noch irgendwie beim einlogen bekommen
+const updateIntervall = 5000;
+
+let apiUserName = null;
+let username = "UNEXPECTED_USERNAME";
+let passwordUser = null;
 
 let chatlogs = null;
 let textinput = null;
 let userbutton = null;
+
 /**
  * Stores the current room displayed to the user
  * @type {String}
  */
 let currentRoom = null;
-window.onload = function startup() {
+/**
+ * This stores the
+ * @type {{room:{
+ *      messages:[String]
+ *      lastSeenLength:{number}
+ * }}}
+ */
+let messageStorage = {};
 
+let clockUpdate = null;
+
+window.onload = function startup() {
     chatlogs = document.getElementById("chatlogs");
     userbutton = document.getElementById("userlist");
     textinput = document.getElementById("textinput");
@@ -50,92 +64,76 @@ window.onload = function startup() {
             let b = document.getElementById("mainM").offsetWidth;
             let r = a + b;
             document.getElementById("topbar").style.width = r + "px";
-
         }
-
     });
-
 
     document.getElementById("submitLog").addEventListener("click", function () {
         let name = document.getElementById("name").value;
-        let password = document.getElementById("password").value;
+        const password = document.getElementById("password").value;
         let last4 = name.slice(-4);
         name = name.slice(0,-4);
         if (name != "" && last4 == "dhbw" && password == "dhbw-pw") {
             let logindiv = document.getElementById("login");
             logindiv.parentNode.removeChild(logindiv);
 
-            //find a best way to store, what you have to store here is:  'dhbw', password, user
-            //Datenspeichern irgendwie
+            apiUserName = last4;
+            passwordUser = password;
+            username = name;
+
             loadlobbys();
         } else {
             alert("Wrong credentials!");
         }
-
-
     });
-
 };
 
-
+/**
+ * Scrolls down the chat that is currently shown
+ */
 function scrolldown() {
     chatlogs.scrollTop = chatlogs.scrollHeight;
 }
 
-function sendtext() {
-    let messageurl = apiurl + "/chats/" + currentRoom;
-    chatlogs.innerHTML = "";
-    //erstellt die user list versteckt
-    let listdiv = document.createElement("div");
-    listdiv.className = "userlist";
-    listdiv.id = "userlister";
-    let listul = document.createElement("ul");
-    listul.id = "listul";
-    listdiv.appendChild(listul);
-    chatlogs.appendChild(listdiv);
-
-
-    //TODO  authenticaon not hardcoded/localstorage
-    let messagerequest = new Request(messageurl, {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Basic ' + btoa('dhbw:dhbw-pw'),
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "roomID": currentRoom,
-            "user": username,
-            "message": textinput.value
-        })
-    });
-    fetch(messagerequest)
-        .then(function (resp) {
-            return resp.json();
-        })
-        .then(function (data) {
-            data.forEach(function (item) {
-                let usernameS = item.user;
-                let textS = item.message;
-
-                let timestamp = new Date(item.timestamp);
-                let h = timestamp.getHours();
-                let m = timestamp.getMinutes();
-                if (m < 10) {
-                    m = "0" + m;
-                }
-                let timeS = h + ":" + m;
-
-                buildmessage(usernameS, textS, timeS);
-            });
-
-            scrolldown();
-        })
-        .catch(function (error) {
-            console.log("Error in sending message:" + error);
-        });
+/**
+ * Generates the value vor the basic authorisation.
+ *
+ * @return {string}
+ */
+function getBasicAuthHeader() {
+    return 'Basic ' + btoa(apiUserName + ':' + passwordUser);
 }
 
+/**
+ * Generates the message from the object the api has produced
+ * @param {{user:{String}, message:{String}, timestamp:{number}}} item
+ */
+function buildMessageFromAPI(item) {
+    const usernameS = item.user;
+    const textS = item.message;
+
+    const timestamp = new Date(item.timestamp);
+    let h = timestamp.getHours();
+    let m = timestamp.getMinutes();
+    if (m < 10) {
+        m = '0' + m;
+    }
+    if (h < 10) {
+        h = '0' + m;
+    }
+    let timeS = h + ":" + m;
+
+    buildmessage(usernameS, textS, timeS);
+}
+
+/**
+ * Build a message and adds it to the DOM in the current displayed chat.
+ *
+ * Should be called by buildMessageFromAPI()
+ *
+ * @param {String} usernameS
+ * @param {String} textS
+ * @param {String} timeS
+ */
 function buildmessage(usernameS, textS, timeS) {
     let divchat = document.createElement("div");
     if (usernameS == username) {
@@ -156,13 +154,50 @@ function buildmessage(usernameS, textS, timeS) {
     pchat.insertBefore(szeit, pchat.firstChild);
     pchat.insertBefore(sname, pchat.firstChild);
     chatlogs.appendChild(divchat);
-
 }
 
+/**
+ * Send the text that is in the text field "textinput"
+ */
+function sendtext() {
+    const chatMessage = textinput.value;
+    const storedCurrentRoom = currentRoom; //store the current Room to not be changed by the user while request ist running
+    if (chatMessage != '') {
+        const messageurl = apiurl + "/chats/" + storedCurrentRoom;
+        const messagerequest = new Request(messageurl, {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Basic ' + btoa('dhbw:dhbw-pw'),
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "roomID": currentRoom,
+                "user": username,
+                "message": textinput.value
+            })
+        });
+        fetch(messagerequest)
+            .then(function (resp) {
+                return resp.json();
+            })
+            .then(function (data) {
+                messageStorage[storedCurrentRoom].messages = data;
+                updateSreenData();
+                scrolldown();
+            })
+            .catch(function (error) {
+                console.error("Error in sending message:" + error);
+            });
+    }
+}
 
-function loadmessage(room) {
-
-    let messageurl = apiurl + "/chats/" + room;
+/**
+ * Resets the currently shown chatlog and displays the room specified
+ * @param {String} room
+ */
+function displayAllMessages(room) {
+    currentRoom = room;
     chatlogs.innerHTML = "";
     let listdiv = document.createElement("div");
     listdiv.className = "userlist";
@@ -172,51 +207,91 @@ function loadmessage(room) {
     listdiv.appendChild(listul);
     chatlogs.appendChild(listdiv);
 
+    messageStorage[room].lastSeenLength = messageStorage[room].messages.length;
+    messageStorage[room].messages.forEach(buildMessageFromAPI);
+    scrolldown();
 
-    //TODO  authenticaon not hardcoded/localstorage
+    updateSreenData();
+}
+
+/**
+ * updates the ScreenData for the currentRoom displayed and the unread messages Counter.
+ * This is done with the data provided in messageStorage
+ */
+function updateSreenData() {
+    let lobbyA = document.getElementsByClassName("lobbyname");
+
+    if (currentRoom === null) {
+        throw new Error('Could not display null room');
+    }
+
+    for (let i = 0; i < lobbyA.length; i++) {
+        if (messageStorage[lobbyA[i].innerHTML] === undefined) {
+            continue;
+        }
+
+        if (lobbyA[i].innerHTML === currentRoom) {
+            for (let j = messageStorage[lobbyA[i].innerHTML].lastSeenLength; j < messageStorage[lobbyA[i].innerHTML].messages.length; j++) {
+                buildMessageFromAPI(messageStorage[lobbyA[i].innerHTML].messages[j]);
+            }
+            messageStorage[lobbyA[i].innerHTML].lastSeenLength = messageStorage[lobbyA[i].innerHTML].messages.length;
+        } else {
+            lobbyA[i].parentNode.childNodes[1].innerHTML = messageStorage[lobbyA[i].innerHTML].messages.length - messageStorage[lobbyA[i].innerHTML].lastSeenLength;
+        }
+    }
+}
+
+/**
+ * Updates the messages stored for this room and
+ * @param room
+ * @param data
+ */
+function updateRoomMessages(room, data) {
+    if (messageStorage[room] === undefined) {
+        messageStorage[room] = {
+            messages: data,
+            lastSeenLength: 0
+        };
+    } else {
+        messageStorage[room].messages = data;
+    }
+    if (currentRoom === null) {
+        switchlobby(room);
+    }
+
+    updateSreenData();
+}
+/**
+ * Requests the messages for the specified room and stores it in to messageStorage
+ * @param room
+ */
+function loadmessage(room) {
+    let messageurl = apiurl + "/chats/" + room;
+
     let messagerequest = new Request(messageurl, {
         method: 'GET',
         headers: {
-            'Authorization': 'Basic ' + btoa('dhbw:dhbw-pw')
+            'Authorization': getBasicAuthHeader()
         }
-
     });
     fetch(messagerequest)
         .then(function (resp) {
             return resp.json();
         })
         .then(function (data) {
-            data.forEach(function (item) {
-                let usernameS = item.user;
-                let textS = item.message;
-
-                let timestamp = new Date(item.timestamp);
-                let h = timestamp.getHours();
-                let m = timestamp.getMinutes();
-                if (m < 10) {
-                    m = "0" + m;
-                }
-                let timeS = h + ":" + m;
-
-
-                buildmessage(usernameS, textS, timeS);
-            });
-
-            scrolldown();
-
-
+            updateRoomMessages(room, data);
         })
         .catch(function (error) {
-            console.log("Error in loadlobbys:" + error);
+            console.error("Error in loadlobbys:" + error);
         });
-
 }
 
-
+/**
+ * TODO write JSDoc
+ */
 function listuser() {
     document.getElementById("userlister").style.width = "25%";
     userbutton.style.backgroundColor = "#E0C65B";
-
 
     let userurl = apiurl + "/chats/" + document.getElementById("chatheader").innerHTML;
 
@@ -224,7 +299,7 @@ function listuser() {
     let userrequest = new Request(userurl, {
         method: 'GET',
         headers: {
-            'Authorization': 'Basic ' + btoa('dhbw:dhbw-pw')
+            'Authorization': getBasicAuthHeader()
         }
 
     });
@@ -253,28 +328,34 @@ function listuser() {
 
         })
         .catch(function (error) {
-            console.log("Error in loadlobbys:" + error);
+            console.error("Error in loadlobbys:" + error);
         });
-
-
 }
+
+/**
+ * TODO write JSDoc
+ */
 function delistuser() {
     document.getElementById("userlister").style.width = "0";
     userbutton.style.backgroundColor = "#F2D769";
-
 }
 
-
+/**
+ * Loads all lobbys form the API and updates the data stored in the messageStorage for every room
+ */
 function loadlobbys() {
+    if (clockUpdate === null) {
+        clockUpdate = setInterval(loadlobbys, updateIntervall);
+    }
+
     let roomurl = apiurl + "/chats";
 
-    //request object TODO authenticaon not hardcoded/localstorage
+    //request object
     let roomrequest = new Request(roomurl, {
         method: 'GET',
         headers: {
-            'Authorization': 'Basic ' + btoa('dhbw:dhbw-pw')
+            'Authorization': getBasicAuthHeader()
         }
-
     });
 
     fetch(roomrequest)
@@ -282,49 +363,51 @@ function loadlobbys() {
             return resp.json();
         })
         .then(function (data) {
-
+            document.getElementById('lobbylogs').innerHTML = '';
             data.forEach(function (item) {
-                let divbutton = document.createElement("div");
-                divbutton.className = "lobby";
-                let newbutton = document.createElement("button");
-                newbutton.className = "lobbyname";
+                const divbutton = document.createElement('div');
+                divbutton.className = 'lobby';
+
+                const newbutton = document.createElement('button');
+                newbutton.className = 'lobbyname';
                 newbutton.onclick = function () {
                     switchlobby(this.innerHTML);
                 };
                 newbutton.innerHTML = item;
-                divbutton.appendChild(newbutton);
-                document.getElementById("lobbylogs").appendChild(divbutton);
-            });
-            switchlobby(data[0]);
 
+                const unreadMessageSpan = document.createElement('span');
+                unreadMessageSpan.className = 'lobbyMessagesUnread';
+                unreadMessageSpan.innerHTML = '0';
+
+                divbutton.appendChild(newbutton);
+                divbutton.appendChild(unreadMessageSpan);
+                document.getElementById('lobbylogs').appendChild(divbutton);
+                loadmessage(item);
+            });
         })
         .catch(function (error) {
-            console.log("Error in loadlobbys:" + error);
+            console.error("Error in loadlobbys:" + error);
         });
-
-
 }
 
-
+/**
+ * Switches to the specified lobby thus removing all currently displayed messages and displaying the new ones
+ * @param {String} name
+ */
 function switchlobby(name) {
-
     //Html element anpassen
     let lobbyA = document.getElementsByClassName("lobbyname");
     let element;
     for (let i = 0; i < lobbyA.length; i++) {
         lobbyA[i].style.backgroundColor = "#F2D769";
-        if (lobbyA[i].innerHTML == name) {
+        if (lobbyA[i].innerHTML === name) {
             element = lobbyA[i];
         }
     }
     element.style.backgroundColor = "#E0C65B";
     document.getElementById("chatheader").innerHTML = name;
 
-    loadmessage(name);
-    currentRoom = name;
-    //TODO lade alle nachrichten, lade alle user
-
-
+    displayAllMessages(name);
 }
 
 
